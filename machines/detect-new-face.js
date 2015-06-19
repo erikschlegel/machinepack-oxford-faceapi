6f7789cab59c5af559d457011b9a6333c0e326cc
Erik Schlegel <erik.schlegel@gmail.com>
2015-06-18 20:14:22 -0400

    adding new machines

--- machines/detect-new-face.js
+++ machines/detect-new-face.js
@@ -1,23 +1,25 @@
 module.exports = {
-  friendlyName: 'find similar faces',
-  description: 'Detects human faces in an image and returns face locations, face landmarks, and optional attributes including head-pose, gender, and age.',
+  friendlyName: 'detect new face',
+  description: 'loads a new image with faces to the face library',
   cacheable: false,
   sync: false,
   idempotent: false,
   inputs: {
   },
 
-
   exits: {
     success: {
       variableName: 'result',
       description: 'Done.',
     },
-  },
 
+  },
 
   fn: function (inputs,exits
   /**/) {
     return exits.success();
   },
+
+
+
 };
